757d6485a6b79d378006b77c449e63af5a5ed0a3
Bui Toan <toan.bui@crmi.vn>
2025-12-03 15:05:07 +0700

    fix

--- src/pages/profile/AccountSetting.tsx
+++ src/pages/profile/AccountSetting.tsx
@@ -49,7 +49,7 @@ const AccountSettingsPage = () => {
       let result = await Login({
         "platform": "ios",
         "type": "phone",
-        "value":"+84"+ context?.state?.user?.phone,
+        "value": context?.state?.user?.phone,
         "password": pin
     })
     if(result.access_token){
