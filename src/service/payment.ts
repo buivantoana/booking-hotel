623439fdad0be42feff094e90cabf1168ff848ae
buivantoana <toanbui219@gmail.com>
2025-12-08 22:24:15 +0700

    fix check payment tokentoken

--- src/service/payment.ts
+++ src/service/payment.ts
@@ -4,7 +4,16 @@ import api from "../core/api";
 
 export async function getStatusPayment(id) {
   try {
-    const response = await api.get(`/payment/status/${id}`);
+    let token = localStorage.getItem("access_token");
+    let headers = {};
+    if (token) {
+      headers = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+    }
+    const response = await api.get(`/payment/status/${id}`, headers);
     return response.data;
   } catch (error: any) {
     if (error.response) {
